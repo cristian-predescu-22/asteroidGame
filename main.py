0d28c7cbd0e2771a9121f5bfb894ecf175d010d7
Cristian <cristipredescu21@yahoo.com>
2025-11-13 18:26:24 +0000

    Added logs, pygame window for game, and event to quit

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
+
+pygame.init()
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
 
 def main():
+
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
+        pygame.Surface.fill(screen, "black")
+        pygame.display.flip()
+
     print("Starting Asteroids with pygame version: VERSION")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
